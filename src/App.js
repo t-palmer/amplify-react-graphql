0424c5a2a324b65c6763d8fd51a792a4624a0767
t-palmer <tpalmer@fujitsu.com>
2023-03-18 18:13:10 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +49,4 @@ function App() {
 }
 
 export default App;
+*/
